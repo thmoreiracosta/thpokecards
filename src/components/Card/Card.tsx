28e97d00134fd5a5f4b3f88a20be372bc31aa045
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-31 12:12:22 -0300

    Complete Project

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from '@apollo/client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import seta from '../../assets/seta.png'
 import './style.css'
 
@@ -32,33 +32,40 @@ export interface GetPokedexResponse {
 
 export default function Card () {
   const { data } = useQuery<GetPokedexResponse>(GET_POKEDEX_QUERY)
-  
-  const array = data?.pokedexs;  
-  let i = (array?.length);
-  console.log(i)
-  const [card, setCard] = useState(0) 
-  
 
-     
-    
-    return (
-      <>
+  const array = data?.pokedexs
+  console.log(array)
+  const [card, setCard] = useState(0)  
+
+  function passIncrementCard () {
+    if (array?.length != undefined) {
+
+      if(card < array?.length - 1) {
+        setCard(card => card + 1)
+      } else {
+        setCard(card => card = 0)
+      }      
+    } 
+}
+
+  return (
+    <>
       <div className='slider-container'>
         <div>
           <button
             className='slider-container-btn-1'
             type='button'
-            onClick={() => card > 0 ? setCard((card) => card - 1) : undefined}
-            
-            >
+            onClick={() => (card > 0 ? setCard(card => card - 1) : undefined)}
+          >
             <img src={seta} alt='seta-voltar' />
           </button>
         </div>
         <ul className='list-pokemons'>
           <li
+            id="list-card"
             key={data?.pokedexs[card].name}
-            className='card-item selected background-2'
-            >
+            className={`card-item selected background-${card}`}
+          >
             <div className='card-info'>
               <h2>{data?.pokedexs[card].name}</h2>
               <p>HP {data?.pokedexs[card].hp}</p>
@@ -67,7 +74,7 @@ export default function Card () {
               <img
                 src={data?.pokedexs[card].imagePokemon.url}
                 alt={data?.pokedexs[card].name}
-                />
+              />
             </div>
             <div className='card-description'>
               <h3>Descrição</h3>
@@ -80,14 +87,12 @@ export default function Card () {
           <button
             className='slider-container-btn-2'
             type='button'
-            onClick={() => card >= 0 ? setCard((card) => card + 1) : undefined}
-            
-            >
+            onClick={() => passIncrementCard ()}
+          >
             <img src={seta} alt='seta-voltar' />
           </button>
         </div>
       </div>
-    
     </>
   )
 }
